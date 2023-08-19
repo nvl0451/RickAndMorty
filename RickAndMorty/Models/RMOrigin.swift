8f9d231aabc6c727081967c878ec2b98b7d42ca9
Андрей Королев <korolevsolutions@MacBook-Pro-Andrej.local>
2023-08-19 06:55:07 +0300

    Add models

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Андрей Королев on 19.08.2023.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Андрей Королев on 19.08.2023.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
