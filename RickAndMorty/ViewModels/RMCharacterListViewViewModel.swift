ee0e61ad005c43ae7f6f10ddcba309cd130ac183
Андрей Королев <korolevsolutions@MacBook-Pro-Andrej.local>
2023-08-20 00:48:41 +0300

    Char View Progress

---
+++ RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  RMCharacterListViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Андрей Королев on 19.08.2023.
+//
+
+import Foundation
+import UIKit
+
+final class RMCharacterListViewViewModel: NSObject {
+    func fetchCharacters() {
+        RMService.shared.execute(.listCharactersRequests,
+                                 expecting: RMGetAllCharactersResponse.self) { result in
+            switch result {
+            case .success(let model):
+                print("Total: " + String(model.info.count))
+                print("Page result count: " + String(model.results.count))
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
+    }
+}
+
+extension RMCharacterListViewViewModel: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemCyan
+        return cell
+    }
+}
+
+extension RMCharacterListViewViewModel: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let bounds = UIScreen.main.bounds
+        let width = (bounds.width - 30)/2
+        return CGSize(width: width, height: width * 1.5)
+    }
+}
