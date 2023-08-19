f770cfd2bb75061205b73bc0a7b1586fc34de787
a korolev <amkorolev@hse.ru>
2023-08-19 04:57:02 +0300

    lmao

---
+++ RickAndMorty/Models/RMCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacter.swift
+//  RickAndMorty
+//
+//  Created by Андрей Королев on 19.08.2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocation.swift
+//  RickAndMorty
+//
+//  Created by Андрей Королев on 19.08.2023.
+//
+
+import Foundation
